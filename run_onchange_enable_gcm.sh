852a30259bf2ea420464a58192ee9d6ca02110e1
Nikko Miu <nikkoamiu@gmail.com>
2025-08-06 15:02:00 -0700

    System install for gcm docker

--- run_onchange_enable_gcm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if which git-credential-manager > /dev/null &> /dev/null; then
-  if [ "$(which git-credential-manager)" != "$(git config --get credential.helper)" ]; then
-    git-credential-manager configure
-  fi
-fi
